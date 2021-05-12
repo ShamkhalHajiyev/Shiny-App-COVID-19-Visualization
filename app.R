47a5161f8e34adf54be4568c778375cadf1f77f7
RezartAbazi <r.abazi@student.uw.edu.pl>
2021-05-12 17:51:27 +0200

    Update app.R

--- app.R
+++ app.R
@@ -19,7 +19,7 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
-                        "Number of bins:",
+                        "Number of BINS:",
                         min = 1,
                         max = 50,
                         value = 30)
